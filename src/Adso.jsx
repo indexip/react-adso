09504ff3f43805116cb19239c59d7065d2632428
unknown <Administrador@DESKTOP-OJBUIDF.akc.co.local>
2024-11-12 20:25:04 -0500

    config

---
+++ src/Adso.jsx
@@ -0,0 +1,14 @@
+import { GestorTarjetas } from "./components/carrito/GestorTarjetas";
+import { TareasComponent } from "./components/tareas/TareasComponent";
+
+
+export const Adso = ()=>{
+
+    return(
+        <>
+        <GestorTarjetas></GestorTarjetas> 
+
+        <TareasComponent/>  
+        </>
+    );
+}
